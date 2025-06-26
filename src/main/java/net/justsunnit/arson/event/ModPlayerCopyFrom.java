3bddaf7b059f5f5c58396e1f0efa87e5f32736fc
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-06-25 17:01:23 -0700

    Cleanup

--- src/main/java/net/justsunnit/arson/event/ModPlayerCopyFrom.java
+++ src/main/java/net/justsunnit/arson/event/ModPlayerCopyFrom.java
@@ -1,8 +1,9 @@
 package net.justsunnit.arson.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.justsunnit.arson.util.IEntityDataSaver;
+import net.justsunnit.arson.util.*;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.justsunnit.arson.*;
 
 public class ModPlayerCopyFrom implements ServerPlayerEvents.CopyFrom {
     @Override
@@ -11,6 +12,6 @@ public class ModPlayerCopyFrom implements ServerPlayerEvents.CopyFrom {
         IEntityDataSaver originalData = (IEntityDataSaver) oldPlayer;
         IEntityDataSaver newData = (IEntityDataSaver) newPlayer;
 
-        newData.getPersistentData().putIntArray("backpos", originalData.getPersistentData().getIntArray("backpos"));
+        newData.getPersistentData().putIntArray("backpos", originalData.getPersistentData().getIntArray("backpos").get());
     }
 }
---
+++ src/main/java/net/justsunnit/arson/event/PlayerConnectionEvent.java
@@ -0,0 +1,7 @@
+package net.justsunnit.arson.event;
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+
+public class PlayerConnectionEvent {
+}
