5e27780ab0b32b0e8cafd5fdd9ededbb8b311785
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-06-26 16:08:59 -0700

    Added Join Buffer

--- src/main/java/net/justsunnit/arson/event/EventRegistries.java
+++ src/main/java/net/justsunnit/arson/event/EventRegistries.java
@@ -1,9 +1,13 @@
 package net.justsunnit.arson.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class EventRegistries {
     public static void registerEvents() {
         ServerPlayerEvents.COPY_FROM.register(new ModPlayerCopyFrom());
+
+        ServerPlayConnectionEvents.JOIN.register(PlayerJoinLeaveEvents::playerJoin);
+        ServerPlayConnectionEvents.DISCONNECT.register(PlayerJoinLeaveEvents::playerLeave);
     }
 }
--- src/main/java/net/justsunnit/arson/event/PlayerJoinLeaveEvents.java
+++ src/main/java/net/justsunnit/arson/event/PlayerJoinLeaveEvents.java
@@ -1,13 +1,56 @@
 package net.justsunnit.arson.event;
 
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.justsunnit.arson.playtime_logging.*;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.justsunnit.arson.Arson;
+import net.justsunnit.arson.automod.BannedData;
+import net.justsunnit.arson.objects.BannedPlayer;
+import net.justsunnit.arson.objects.PlayerPlaytimeData;
+import net.justsunnit.arson.util.JsonSaveHandler;
+import net.justsunnit.arson.util.TextFormatter;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static net.justsunnit.arson.util.PlaytimeLogger.*;
 
 
 public class PlayerJoinLeaveEvents {
-    public static void registerEvents() {
-        ServerPlayConnectionEvents.JOIN.register(PlaytimeLogger::playerJoin);
+    public static void playerJoin(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server){
 
-        ServerPlayConnectionEvents.DISCONNECT.register(PlaytimeLogger::playerLeave);
+        if(BannedData.checkPlayer(handler.getPlayer().getUuidAsString())){
+            BannedPlayer ban = BannedData.getBannedPlayer(handler.getPlayer().getUuidAsString());
+
+            handler.disconnect(Text.literal("You are banned for: " + (ban.timeless ? "inf" : TextFormatter.formatDuration(ban.BanSeconds))
+                            + "\n Reason/Reasons:\n" + ban.Reason.toString()).styled(style -> style.withBold(true)));
+
+            return;
+        }
+
+        playerLoginTimeStamp.put(handler.getPlayer().getName().getLiteralString(), LocalDateTime.now());
+        handler.getPlayer().sendMessage(Text.literal("[ArsonUtils] Logged Login Time: " + LocalDateTime.now().toString()).styled(style -> style.withBold(true).withColor(Formatting.RED)), false);
+    }
+
+    public static void playerLeave(ServerPlayNetworkHandler handler, MinecraftServer server){
+        Duration timeSpent = Duration.between(playerLoginTimeStamp.getOrDefault(handler.getPlayer().getName().toString(),LocalDateTime.now()), LocalDateTime.now());
+        PlayerPlaytimeData data = (PlayerPlaytimeData) JsonSaveHandler.GetPlayerPlaytimeData().getOrDefault(handler.getPlayer().getName().toString(), new PlayerPlaytimeData());
+        data.AddPlaytime(timeSpent.toSeconds());
+        JsonSaveHandler.SavePlayerPlaytimeData(data, handler.getPlayer().getName().toString());
+        Arson.LOGGER.info("[ArsonUtils] Playtime for " + handler.getPlayer().getName().getLiteralString() + " has been saved.");
+
+        playerLoginTimeStamp.remove(handler.getPlayer().getName().getLiteralString());
+
+        BannedPlayer bannedPlayer = new BannedPlayer(
+                handler.getPlayer().getName().toString(),
+                "[Automod] Join/Leave Buffer",
+                LocalDateTime.now(),
+                15L,
+                false
+        );
+
+        BannedData.bufferPlayer(bannedPlayer, handler.getPlayer().getUuidAsString());
     }
 }
