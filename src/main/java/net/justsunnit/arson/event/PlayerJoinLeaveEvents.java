6d70c72bccad33cda27d0eb2bc4dce56d73df22a
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-06-25 17:04:01 -0700

    Actual Cleanup

--- src/main/java/net/justsunnit/arson/event/PlayerJoinLeaveEvents.java
+++ src/main/java/net/justsunnit/arson/event/PlayerJoinLeaveEvents.java
@@ -1,12 +1,11 @@
 package net.justsunnit.arson.event;
 
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.justsunnit.arson.playtime_logging.PlaytimeLogger;
+import net.justsunnit.arson.playtime_logging.*;
 
 
 public class PlayerJoinLeaveEvents {
     public static void registerEvents() {
-
         ServerPlayConnectionEvents.JOIN.register(PlaytimeLogger::playerJoin);
 
         ServerPlayConnectionEvents.DISCONNECT.register(PlaytimeLogger::playerLeave);
