d0d317688ecf41fa681c3d8e3b51a345b93eea94
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-06-26 15:00:36 -0700

    Made Ban Methoad Disconnect Player

--- src/main/java/net/justsunnit/arson/automod/BannedData.java
+++ src/main/java/net/justsunnit/arson/automod/BannedData.java
@@ -3,19 +3,22 @@ package net.justsunnit.arson.automod;
 import net.justsunnit.arson.Arson;
 import net.justsunnit.arson.objects.BannedPlayer;
 import net.justsunnit.arson.util.JsonSaveHandler;
+import net.justsunnit.arson.util.TextFormatter;
+import net.minecraft.text.Text;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.UUID;
 
-public class BannedData {
-    public static HashMap<String, BannedPlayer> bannedPlayers = new HashMap<>();
+public class BannedData { ;
 
-    public static void loadBannedData() {
-        bannedPlayers = JsonSaveHandler.GetBannedPlayers();
+    public static HashMap<String, BannedPlayer> loadBannedData() {
+        return JsonSaveHandler.GetBannedPlayers();
     }
 
     public static boolean checkPlayer(String playerUUID) {
+        HashMap<String, BannedPlayer> bannedPlayers = loadBannedData();
         if(bannedPlayers.containsKey(playerUUID)){
             BannedPlayer bannedPlayer = bannedPlayers.get(playerUUID);
             LocalDateTime now = LocalDateTime.now();
@@ -34,14 +37,15 @@ public class BannedData {
     }
 
     public static BannedPlayer getBannedPlayer(String playerUUID) {
+        HashMap<String, BannedPlayer> bannedPlayers = loadBannedData();
         if(!bannedPlayers.containsKey(playerUUID)) {
             return null; // Player is not banned
         }
         return bannedPlayers.get(playerUUID);
     }
 
-    public static void banPlayer(BannedPlayer player) {
-        String uuid = Arson.server.getPlayerManager().getPlayer(player.Name).getUuidAsString();
+    public static void banPlayer(BannedPlayer player, String uuid) {
+        HashMap<String, BannedPlayer> bannedPlayers = loadBannedData();
         if(bannedPlayers.containsKey(uuid)) {
             BannedPlayer data = bannedPlayers.get(uuid);
             data.BanSeconds += player.BanSeconds;
@@ -51,17 +55,23 @@ public class BannedData {
             bannedPlayers.put(uuid, player);
         }
         JsonSaveHandler.SaveBannedPlayerData(bannedPlayers);
-        Arson.LOGGER.info("[ArsonUtils] Banned player: " + player.Name + " for " + (player.timeless ? "timeless" : Duration.ofSeconds(player.BanSeconds).toString()));
+
+        Arson.server.getPlayerManager().getPlayer(UUID.fromString(uuid)).networkHandler.disconnect(Text.literal(
+                "You are banned for: " + (player.timeless ? "inf" : TextFormatter.formatDuration(player.BanSeconds))
+                + "\n Reason/Reasons:\n" + player.Reason.toString()).styled(style -> style.withBold(true)));
+
+        Arson.LOGGER.info("[ArsonUtils] Banned player: " + player.Name);
     }
 
-    public static void unbanPlayer(String playerUUID) {
-        if(bannedPlayers.containsKey(playerUUID)) {
+    public static boolean unbanPlayer(String playerUUID) {
+        HashMap<String, BannedPlayer> bannedPlayers = loadBannedData();
+        if (bannedPlayers.containsKey(playerUUID)) {
             String playerName = bannedPlayers.get(playerUUID).Name;
             bannedPlayers.remove(playerUUID);
             JsonSaveHandler.SaveBannedPlayerData(bannedPlayers);
             Arson.LOGGER.info("[ArsonUtils] Unbanned player: " + playerName);
-        } else {
-            Arson.LOGGER.warn("[ArsonUtils] Attempted to unban a player that is not banned: " + Arson.server.getPlayerManager().getPlayer(playerUUID).getName().toString());
+            return true;
         }
+        return false;
     }
 }
