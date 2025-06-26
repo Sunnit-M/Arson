4e7c726e297a38ad5da057a263ecea64f06ef240
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-06-26 15:15:56 -0700

    Added Contructor

--- src/main/java/net/justsunnit/arson/objects/BannedPlayer.java
+++ src/main/java/net/justsunnit/arson/objects/BannedPlayer.java
@@ -3,6 +3,14 @@ package net.justsunnit.arson.objects;
 import java.time.LocalDateTime;
 
 public class BannedPlayer {
+    public BannedPlayer (String name, String reason, LocalDateTime banDate, long banSeconds, boolean timeless) {
+        this.Name = name;
+        this.Reason = new StringBuilder(reason);
+        this.BanDate = banDate;
+        this.BanSeconds = banSeconds;
+        this.timeless = timeless;
+    }
+
     public String Name;
     public StringBuilder Reason;
     public LocalDateTime BanDate;
