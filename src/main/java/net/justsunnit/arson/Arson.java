be7cbaaa66c31eaf8097673052f125f0af83872c
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-07-09 13:48:18 -0700

    Added Main EntryPoihnt

---
+++ src/main/java/net/justsunnit/arson/Arson.java
@@ -0,0 +1,12 @@
+package net.justsunnit.arson;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Arson implements ModInitializer {
+    public static final String MOD_ID = "arson";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
