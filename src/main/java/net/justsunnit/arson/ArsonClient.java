8be6e1c1bbe29115ecbd458a0bda37efd16cc590
Sunnit-M <edwinnoblemessinger@icloud.com>
2025-09-03 17:49:00 -0700

    Added key checking

--- src/main/java/net/justsunnit/arson/ArsonClient.java
+++ src/main/java/net/justsunnit/arson/ArsonClient.java
@@ -5,15 +5,21 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.justsunnit.arson.data.ClientStaticData;
 import net.justsunnit.arson.event.EventRegistries;
 
+import java.util.Objects;
+
 public class ArsonClient implements ClientModInitializer {
+
+    public static boolean keyed = false;
+
     @Override
     public void onInitializeClient() {
         FabricLoader.getInstance().getAllMods().forEach(item -> {
-            ClientStaticData.loadedModIDs.add(item.getMetadata().getId());
-
-            Arson.LOGGER.info("Found mod: " + item.getMetadata().getId());
+            if(Objects.equals(item.getMetadata().getId(), "arsonkey")) keyed = true;
+            else ClientStaticData.loadedModIDs.add(item.getMetadata().getId());
         });
 
         EventRegistries.registerEvents_Client();
+
+
     }
 }
